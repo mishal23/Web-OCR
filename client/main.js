f2da50a1cee01b177f20fe301e857b4a5e5711a1
mishal23 <shahmishal1998@gmail.com>
2017-08-27 16:02:16 +0530

    Basic converter working

---
+++ client/main.js
@@ -0,0 +1,38 @@
+var app = angular.module('myapp', ['ngFileUpload']);
+
+app.controller('MyCtrl',['Upload','$window', '$scope','$http',function(Upload,$window,$scope,$http){
+    var vm = this;
+    vm.submit = function(){
+        if (vm.upload_form.file.$valid && vm.file) {
+            vm.upload(vm.file);
+        }
+    };
+    
+    vm.upload = function (file) {
+        Upload.upload({
+            url: 'http://localhost:3000/upload',
+            data: {file:file}
+        }).then(function (resp) {
+            if(resp.data.error_code === 0){
+                $window.alert('File ' + resp.config.data.file.name + ' uploaded.');
+            }
+            else {
+                $window.alert('an error occurred');
+            }
+        }, function (resp) { //catch error
+            console.log('Error status: ' + resp.status);
+            $window.alert('Error status: ' + resp.status);
+        });
+    };
+
+    $scope.convert = function () {
+        console.log("Reached convert");
+        $http.get('/text').then(function (res) {
+            console.log(res.data);
+            $scope.answer = res.data;
+        },function fail() {
+            console.log("Failed");
+        });
+    };
+}]);
+
